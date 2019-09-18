494baf0f65fe8f3397f45f634477a05c6897fca2
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-09-18 21:19:05 +0000

    fixed issues 07

--- exercises/07-String-Concatenation/app.py
+++ exercises/07-String-Concatenation/app.py
@@ -1,9 +1,8 @@
 # Set the values here
-my_var1 = 'Hello'
-my_var2 = 'World'
+
 
 
 
 ## Don't change any of this line
 the_new_string = my_var1+' '+my_var2
-print(the_new_string) 
+print(the_new_string)
