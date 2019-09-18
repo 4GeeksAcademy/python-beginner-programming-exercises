b7b45a876b32cc9b4120d173b958f8f19794726f
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-09-18 16:17:21 +0000

    completed 02

--- exercises/02-Declare-Variables/test.py
+++ exercises/02-Declare-Variables/test.py
@@ -1,21 +1,12 @@
 import io
 import sys
 sys.stdout = buffer = io.StringIO()
-
+import app
 # from app import my_function
 import pytest
 
-@pytest.mark.it('Your code needs to print hello on the console')
+@pytest.mark.it('Your code needs to print Yellow on the console')
 def test_for_file_output(capsys):
     captured = buffer.getvalue()
-    assert captured == "hello\n" #add \n because the console jumps the line on every print
+    assert captured == "Yellow\n" #add \n because the console jumps the line on every print
 
-# @pytest.mark.it('Your function needs to print "Hello Inside Function" on the console')
-# def test_for_function_output(capsys):
-#     my_function()
-#     captured = capsys.readouterr()
-#     assert captured.out == "Hello Inside Function\n"
-
-# @pytest.mark.it('Your function needs to return True')
-# def test_for_function_return(capsys):
-#     assert my_function() == True
