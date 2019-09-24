9935509e22951ba126f80ffcf5f491b28a4a5dfd
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-09-24 19:53:37 +0000

    updated 05

--- exercises/05-User-Inputed-Values/app.py
+++ exercises/05-User-Inputed-Values/app.py
@@ -1,1 +1,5 @@
 age = int(input('What is your age?'))
+# YOUR CODE HERE
+
+# print("Your age is: "+str(int(age)+10))
+print(age+10)
--- exercises/05-User-Inputed-Values/test.py
+++ exercises/05-User-Inputed-Values/test.py
@@ -1,21 +1,25 @@
 import io
 import sys
 sys.stdout = buffer = io.StringIO()
-
+sys.stdin = buffer = input()
 # from app import my_function
 import pytest
+import app
+import os
 
-@pytest.mark.it('Your code needs to print hello on the console')
+# @pytest.mark.it('1. You should print on the console the input value + 10')
+# def test_use_variable_name():
+
+#     f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
+#     content = f.read()
+#     assert content.find("console") > 0
+@pytest.mark.it('3. Your code needs to print Hello World on the console')
 def test_for_file_output(capsys):
     captured = buffer.getvalue()
-    assert captured == "hello\n" #add \n because the console jumps the line on every print
-
-# @pytest.mark.it('Your function needs to print "Hello Inside Function" on the console')
-# def test_for_function_output(capsys):
-#     my_function()
-#     captured = capsys.readouterr()
-#     assert captured.out == "Hello Inside Function\n"
-
-# @pytest.mark.it('Your function needs to return True')
-# def test_for_function_return(capsys):
-#     assert my_function() == True
+    inp = buffer.getvalue() +10
+    assert captured == inp+"\n" #add \n because the console jumps the line on every print
+# @pytest.mark.it('2. You should print on the console the variables_are_cool value ')
+# def test_for_file_output(capsys):
+#     my_result = 2345 *7323
+#     captured = stdin
+#     assert captured == str(my_result)+'\n'
