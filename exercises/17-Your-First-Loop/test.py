6f797989dc3a907aa36b31224463fc9667338d7d
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-09-25 19:27:40 +0000

    updated 17

--- exercises/17-Your-First-Loop/test.py
+++ exercises/17-Your-First-Loop/test.py
@@ -4,11 +4,33 @@ sys.stdout = buffer = io.StringIO()
 
 # from app import my_function
 import pytest
+import os
+import app
+import re
 
-@pytest.mark.it('Your code needs to print hello on the console')
-def test_for_file_output(capsys):
-    captured = buffer.getvalue()
-    assert captured == "hello\n" #add \n because the console jumps the line on every print
+
+@pytest.mark.it("1. You should fix the existing code")
+def test_existingCode():
+    f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
+    content = f.read()
+    # content = [x.strip() for x in content]  ####  With this line of code I removed all the whitespace characters like `\n` at the end of each line
+    # print("$$$$", content)
+    price_variable = (r"def start_counting\(\):	\n"
+	      r"(\s*)for i in range\(101\):\n"
+	      r"(\s*)print\(i\)	\n"
+	      r"(\s+)return i\n\n"
+	      r"start_counting\(\)")
+    assert re.match(price_variable, content)
+
+# @pytest.mark.it('The console should count from 0 to 100')
+# def test_for_file_output(capsys):
+#     def start_counting():
+# 	      for i in range(101):
+# 		        print(i)
+
+#     captured = buffer.getvalue()
+#     print("test",start_counting())
+#     assert captured == str(start_counting())+'\n'
 
 # @pytest.mark.it('Your function needs to print "Hello Inside Function" on the console')
 # def test_for_function_output(capsys):
