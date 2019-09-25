14e0788d16b46ee5280130fc8b0bed932edc59e8
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-09-25 16:11:18 +0000

    updated 13

--- exercises/13-How-Much-The-Wedding-Costs/app.py
+++ exercises/13-How-Much-The-Wedding-Costs/app.py
@@ -1,3 +1,15 @@
-userInput = prompt('How many people are coming to your wedding?');
+# user_input = int(input('How many people are coming to your wedding?'))
+user_input = 99
+price = 0
+if( user_input < 51):
+    price = 4000
+elif (51 < user_input and user_input < 101):
+    price = 10000
+elif (101 < user_input and user_input < 201):
+    price = 15000
+else:
+    price = 20000
 
-# your code here
+
+# Your code above here
+print('Your wedding will cost '+str(price)+' dollars');
--- exercises/13-How-Much-The-Wedding-Costs/test.py
+++ exercises/13-How-Much-The-Wedding-Costs/test.py
@@ -4,8 +4,39 @@ sys.stdout = buffer = io.StringIO()
 
 # from app import my_function
 import pytest
+import app
+import os
+import re
 
-@pytest.mark.it('Your code needs to print hello on the console')
+
+@pytest.mark.it("1. You should not delete or change the existing code")
+def test_existing_code():
+    f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
+    content = f.readlines()
+    print("####",content[0])
+    original_input = r"# user_input = int\(input\('How many people are coming to your wedding\?'\)\)"
+    assert re.match(original_input, content[0])
+    regex = r"print\('Your wedding will cost '\+str\(price\)\+' dollars'\);"
+    assert re.match(regex, content[(len(content)-1)])
+
+@pytest.mark.it("2. You should create a price variable before the if statement")
+def test_existingPriceVariable():
+    f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
+    content = f.readlines()
+    content = [x.strip() for x in content]  ####  With this line of code I removed all the whitespace characters like `\n` at the end of each line
+    price_variable = r"price = (.+)"
+    assert re.match(price_variable, content[2])
+    # for price in content:
+    #     if price == price_variable:
+    #         assert re.match(price_variable, price)
+    # for price in content:
+    #     price_index = content.index(price)
+    #     if re.match(price_variable, price_index):
+    #         if price_index < 3:
+
+
+
+@pytest.mark.it('3. Your code needs to print the correct outpu on the console')
 def test_for_file_output(capsys):
     captured = buffer.getvalue()
     assert captured == "hello\n" #add \n because the console jumps the line on every print
