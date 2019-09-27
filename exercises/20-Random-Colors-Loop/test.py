ee9965f25433cbd189656bb69bbd9574be2dcf82
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-09-27 21:06:12 +0000

    updated 20

--- exercises/20-Random-Colors-Loop/test.py
+++ exercises/20-Random-Colors-Loop/test.py
@@ -4,11 +4,19 @@ sys.stdout = buffer = io.StringIO()
 
 # from app import my_function
 import pytest
+import app
+import os
+import re
 
-@pytest.mark.it('Your code needs to print hello on the console')
+@pytest.mark.it('STEP 3. Your code needs to print the correct output on the console')
 def test_for_file_output(capsys):
     captured = buffer.getvalue()
-    assert captured == "hello\n" #add \n because the console jumps the line on every print
+    f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
+    content = f.readlines()
+    content = [x.strip() for x in content]
+    print("@@@@", content)
+    regex = r"print\(get_allStudentColors\(\)\)"
+    assert re.match(regex, content[(len(content)-1)])
 
 # @pytest.mark.it('Your function needs to print "Hello Inside Function" on the console')
 # def test_for_function_output(capsys):
