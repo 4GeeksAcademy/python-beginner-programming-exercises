8cafcd7c382016ff1a6c47e7ffe26e9544cbdc1f
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-10-02 17:53:32 +0000

    fixed issues 05

--- exercises/05-User-Inputed-Values/test.py
+++ exercises/05-User-Inputed-Values/test.py
@@ -1,25 +1,14 @@
-import io
-import sys
-sys.stdout = buffer = io.StringIO()
-sys.stdin = buffer = input()
-# from app import my_function
-import pytest
-import app
-import os
+import pytest,os,re,io,sys, mock, json
 
-# @pytest.mark.it('1. You should print on the console the input value + 10')
-# def test_use_variable_name():
-
-#     f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
-#     content = f.read()
-#     assert content.find("console") > 0
-@pytest.mark.it('3. Your code needs to print Hello World on the console')
-def test_for_file_output(capsys):
-    captured = buffer.getvalue()
-    inp = buffer.getvalue() +10
-    assert captured == inp+"\n" #add \n because the console jumps the line on every print
-# @pytest.mark.it('2. You should print on the console the variables_are_cool value ')
-# def test_for_file_output(capsys):
-#     my_result = 2345 *7323
-#     captured = stdin
-#     assert captured == str(my_result)+'\n'
+@pytest.mark.it("1. You should print on the console the input value + 10")
+def test_t(stdin):
+    _input = json.loads(stdin)
+    print("####", _input[0])
+    my_testString = int(_input[0]) + 10
+    print("$$$$$:",int(my_testString) + 10)
+    with mock.patch('builtins.input', lambda x: _input.pop()):
+      # f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
+      sys.stdout = buffer = io.StringIO()
+      import app
+      captured = buffer.getvalue()
+      assert captured == "Your age is: " + str(my_testString)+'\n'
