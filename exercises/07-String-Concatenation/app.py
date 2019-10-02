7da8203d3adc54a2bfa163d4aa3392cc6bd425a6
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-10-02 18:04:09 +0000

    fixed issues 07

--- exercises/07-String-Concatenation/app.py
+++ exercises/07-String-Concatenation/app.py
@@ -1,8 +1,6 @@
 # Set the values here
 
 
-
-
-## Don't change any of this line
+## Don't change below this line
 the_new_string = my_var1+' '+my_var2
 print(the_new_string)
--- exercises/07-String-Concatenation/test.py
+++ exercises/07-String-Concatenation/test.py
@@ -22,6 +22,7 @@ def test_use_my_var2():
     assert content.find("my_var2") > 0
 @pytest.mark.it('3. Your code needs to print Hello World on the console')
 def test_for_file_output(capsys):
+
     captured = buffer.getvalue()
     assert captured == "Hello World\n" #add \n because the console jumps the line on every print
 
