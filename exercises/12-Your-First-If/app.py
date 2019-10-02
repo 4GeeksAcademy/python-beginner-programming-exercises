e0d5a2a090ed86d3e9edcc8b02c98307c00b2df1
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-10-02 19:42:36 +0000

    fixed issues 12

--- exercises/12-Your-First-If/app.py
+++ exercises/12-Your-First-If/app.py
@@ -1,9 +1,4 @@
 
 total = int(input('How much money do you have in your pocket\n'))
 
-if (total > 100):
-    print("Give me your money!")
-elif (total > 50):
-    print("Buy me some coffee you cheap!")
-elif (total < 51):
-    print("You are a poor guy, go away!")
+# YOUR CODE HERE
