1742933573adc17a39fac5cbd9147508efbeb0ef
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-10-02 20:06:18 +0000

    fixed issues 13

--- exercises/13-How-Much-The-Wedding-Costs/app.py
+++ exercises/13-How-Much-The-Wedding-Costs/app.py
@@ -1,15 +1,7 @@
-# user_input = int(input('How many people are coming to your wedding?'))
-user_input = 99
-price = 0
-if( user_input < 51):
-    price = 4000
-elif (51 < user_input and user_input < 101):
-    price = 10000
-elif (101 < user_input and user_input < 201):
-    price = 15000
-else:
-    price = 20000
+user_input = int(input('How many people are coming to your wedding?\n'))
 
 
-# Your code above here 
-print('Your wedding will cost '+str(price)+' dollars');
+
+
+# Your code above here
+print('Your wedding will cost '+str(price)+' dollars')
--- exercises/13-How-Much-The-Wedding-Costs/test.py
+++ exercises/13-How-Much-The-Wedding-Costs/test.py
@@ -1,55 +1,31 @@
-import io
-import sys
-
 # from app import my_function
-import pytest
-import app
-import os
-import re
+import pytest,os,re,io,sys, mock, json
 
+@pytest.mark.it("1. The console should return the correct string for each condition")
+def test_t(stdin):
+    _input = json.loads(stdin)
+    print("####", _input)
+    with mock.patch('builtins.input', lambda x: _input[0]):
+        if int(_input[0]) > 100 and int(_input[0]) < 200:
+            # f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
+            sys.stdout = buffer = io.StringIO()
+            import app
+            price = 15000
 
-@pytest.mark.it("1. You should not delete or change the existing code")
-def test_existing_code():
-    f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
-    content = f.readlines()
+            "Your wedding will cost "+str(price)+" dollars\n" == buffer.getvalue()
+        elif int(_input[0]) < 101 and int(_input[0]) > 50:
+            sys.stdout = buffer = io.StringIO()
+            import app
+            price = 10000
 
-    original_input = r"# user_input = int\(input\('How many people are coming to your wedding\?'\)\)"
-    assert re.match(original_input, content[0])
-    regex = r"print\('Your wedding will cost '\+str\(price\)\+' dollars'\);"
-    assert re.match(regex, content[(len(content)-1)])
-
-@pytest.mark.it("2. You should create a price variable before the if statement")
-def test_existingPriceVariable():
-    f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
-    content = f.readlines()
-    content = [x.strip() for x in content]  ####  With this line of code I removed all the whitespace characters like `\n` at the end of each line
-    price_variable = r"price = (.+)"
-    assert re.match(price_variable, content[2])
-    # for price in content:
-    #     if price == price_variable:
-    #         assert re.match(price_variable, price)
-    # for price in content:
-    #     price_index = content.index(price)
-    #     if re.match(price_variable, price_index):
-    #         if price_index < 3:
-
-
-
-@pytest.mark.it('3. Your code needs to print the correct outpu on the console')
-def test_for_file_output():
-    # sys.stdout = buffer = io.StringIO()
-    test =sys.stdin.read()
-    # captured = buffer.getvalue()
-    # print("####",buffer.getvalue())
-    print("$$$$",test)
-    # assert captured == "hello\n" #add \n because the console jumps the line on every print
-
-# @pytest.mark.it('Your function needs to print "Hello Inside Function" on the console')
-# def test_for_function_output(capsys):
-#     my_function()
-#     captured = capsys.readouterr()
-#     assert captured.out == "Hello Inside Function\n"
-
-# @pytest.mark.it('Your function needs to return True')
-# def test_for_function_return(capsys):
-#     assert my_function() == True
+            assert "Your wedding will cost "+str(price)+" dollars\n" == buffer.getvalue()
+        elif int(_input[0]) < 50:
+            sys.stdout = buffer = io.StringIO()
+            import app
+            price = 4000
+            "Your wedding will cost "+str(price)+" dollars\n" == buffer.getvalue()
+        elif int(_input[0]) > 200:
+            sys.stdout = buffer = io.StringIO()
+            import app
+            price = 20000
+            "Your wedding will cost "+str(price)+" dollars\n" == buffer.getvalue()
