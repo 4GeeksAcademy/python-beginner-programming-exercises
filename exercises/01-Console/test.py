0988d98893c20c4147f6f58b53f3f30fa010d1b6
Alejandro Sanchez <aalejo@gmail.com>
2020-01-07 22:01:42 +0000

    more firendly test report

--- exercises/01-Console/test.py
+++ exercises/01-Console/test.py
@@ -6,16 +6,16 @@ import re
 import os
 import pytest
 
-@pytest.mark.it('1. Your code needs to print Hello World! on the console')
+@pytest.mark.it('1. You need to use the function print()')
 def test_for_file_output(capsys):
-    f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
-    content = f.readlines()
-    content = [x.strip() for x in content]
-    my_print = [s for s in content if "print" in s]
-    my_printIndex = content.index(my_print[0])
-    # print(my_print_index)
-    regex = r"print\(\"Hello World!\"\)"
-    assert re.match(regex, content[my_printIndex])
+    path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        regex = r"print\("
+        assert re.match(regex, content) != None
+
+@pytest.mark.it('2. Your code needs to print Hello World! on the console')
+def test_for_console_log(capsys):
     captured = buffer.getvalue()
     assert captured == "Hello World!\n" #add \n because the console jumps the line on every print
 
