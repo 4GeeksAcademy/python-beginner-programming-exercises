b971198e2779ae788835c4f3c3ac8d091f4d5b62
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-08-11 17:25:35 +0000

    Issue #303 fixed

--- exercises/18-The-Beatles/test.py
+++ exercises/18-The-Beatles/test.py
@@ -1,29 +1,22 @@
 import io
 import sys
 sys.stdout = buffer = io.StringIO()
-from app import sing
 
+# from app import my_function
+from app import sing
 import pytest
-import os
 import app
 import re
+import os
 
+@pytest.mark.it("You should declare a function named sing and call it in the correct way")
+def test_function_sing_exists(app):
+    try:
+        app.sing
+    except AttributeError:
+        raise AttributeError("The function 'sing' should exist on app.py")
 
-@pytest.mark.it("1. You should declare a function named sing and call it in the correct way")
-def test_functionSing():
-    f = open(os.path.dirname(os.path.abspath(__file__))+ '/app.py')
-    content = f.readlines()
-    content = [x.strip() for x in content]
-    my_funcName = [s for s in content if "def sing():" in s]
-    my_funcNameVar = content.index(my_funcName[0])
-    regex = r"def sing\(\):"
-    my_print = [s for s in content[2:] if "sing()" in s]
-    my_printVar = content.index(my_print[0])
-    regexPrint = r"sing\(\)"
-    assert re.match(regex, content[my_funcNameVar])
-    assert re.match(regexPrint, content[my_printVar])
-
-@pytest.mark.it('2. Your function needs to print the correct output')
+@pytest.mark.it('Your function needs to print the correct output')
 def test_for_function_output(capsys):
     sing()
     captured = capsys.readouterr()
