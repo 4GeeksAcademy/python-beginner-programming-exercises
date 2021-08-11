da6762c8af3a8034a7a3c5dd04dde2664f553d78
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-08-11 16:04:59 +0000

    Issue #292 fixed

--- exercises/09-Random-Numbers/test.py
+++ exercises/09-Random-Numbers/test.py
@@ -7,12 +7,31 @@ import pytest
 import os
 import re
 
-@pytest.mark.it("You should update only line 5 using randint()")
+@pytest.mark.it("You should edit the function randint() to generate a random number between 1 and 10")
 def test_conditional():
     path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
     with open(path, 'r') as content_file:
         content = content_file.read()
-        pattern = r"random_number(\s*)=(\s*)random\.rand\w+\(1,10\)"
+        pattern = r"random_number(\s*)=(\s*)random\.rand\w+(\s)*\((\s)*1(\s)*,(\s)*10(\s)*\)"
         regex = re.compile(pattern)
         assert bool(regex.search(content)) == True
 
+@pytest.mark.it("You should only change line 5")
+def test_only_change_line_5():
+    path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        line_1 = r"import(\s)random"
+        line_2 = r"def(\s)get_randomInt\((\s*)\)(\s*)\:"
+        line_3 = r"return(\s)random_number"
+        line_4 = r"print(\s*)\(get_randomInt(\s*)\((\s*)\)(\s*)\)"
+
+        regex = re.compile(line_1)
+        regex = re.compile(line_2)
+        regex = re.compile(line_3)
+        regex = re.compile(line_4)
+
+        assert bool(regex.search(content)) == True
+        assert bool(regex.search(content)) == True
+        assert bool(regex.search(content)) == True
+        assert bool(regex.search(content)) == True
