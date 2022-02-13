c84475178a50047492d227994b848df8e93960a7
Martín Suárez <ms.prgramador@gmail.com>
2022-02-13 03:45:18 +0000

    16-Random-Colors-Loop

--- exercises/16-Random-Colors-Loop/test.py
+++ exercises/16-Random-Colors-Loop/test.py
@@ -7,12 +7,35 @@ import pytest
 import app
 import os
 import re
+import mock
 
-@pytest.mark.it('Print the correct output on the console')
+@pytest.mark.it('The function get_allStudentColors should exist')
+def test_function_existence():
+    try:
+        app.get_allStudentColors
+    except AttributeError:
+        raise AttributeError('The function get_allStudentColors should exist')
+
+@pytest.mark.it('The function get_color should exist')
+def test_function_existence():
+    try:
+        app.get_color
+    except AttributeError:
+        raise AttributeError('The function get_color should exist')
+
+@pytest.mark.it('The function get_allStudentColors shuould call get_colors inside')
+def test_function_inside_studentColors():
+    with mock.patch('app.get_color') as mocked_get_color:
+        from app import get_allStudentColors
+        get_allStudentColors()
+        mocked_get_color.assert_called_with(r"[red,yellow,blue,green]")
+
+@pytest.mark.it('Print the function on the console')
 def test_for_file_output(capsys):
     path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
     with open(path, 'r') as content_file:
         content = content_file.read()
-        pattern = r"print\(get_allStudentColors\(\)\)"
+        pattern = r"print\s*\(\*get_allStudentColors\(\)\s*\)"
         regex = re.compile(pattern)
         assert bool(regex.search(content)) == True
+
