0dea116364df15a058c8dd4bad0fe3a2f19eac3b
Martín Suárez <ms.prgramador@gmail.com>
2022-03-16 17:55:56 +0000

    16-Random-Colors-Loop

--- exercises/16-Random-Colors-Loop/test.py
+++ exercises/16-Random-Colors-Loop/test.py
@@ -7,35 +7,35 @@ import pytest
 import app
 import os
 import re
-import mock
 
-@pytest.mark.it('The function get_allStudentColors should exist')
-def test_function_existence():
-    try:
-        app.get_allStudentColors
-    except AttributeError:
-        raise AttributeError('The function get_allStudentColors should exist')
-
-@pytest.mark.it('The function get_color should exist')
-def test_function_existence():
+@pytest.mark.it('Function get_color should exist')
+def test_get_color_exists(capsys, app):
     try:
         app.get_color
     except AttributeError:
-        raise AttributeError('The function get_color should exist')
+        raise AttributeError("The function 'get_color' should exist on app.py")
+@pytest.mark.it('Function get_allStudentColors should exist')
+def test_get_allStudentColors_exists(capsys, app):
+    try:
+        app.get_allStudentColors
+    except AttributeError:
+        raise AttributeError("The function 'get_allStudentColors' should exist on app.py")
 
-@pytest.mark.it('The function get_allStudentColors shuould call get_colors inside')
-def test_function_inside_studentColors():
-    with mock.patch('app.get_color') as mocked_get_color:
-        from app import get_allStudentColors
-        get_allStudentColors()
-        mocked_get_color.assert_called_with(r"[red,yellow,blue,green]")
+@pytest.mark.it('Function get_allStudentColors should return ten colors')
+def test_ten_colors(capsys, app):
+    result = app.get_allStudentColors()
+    assert len(result) == 10
 
-@pytest.mark.it('Print the function on the console')
-def test_for_file_output(capsys):
+@pytest.mark.it("Function get_allStudentColors shouldn't return an array with the black color")
+def test_black_in_array(capsys, app):
+    result = app.get_allStudentColors()
+    assert result.count("black") == 0
+
+@pytest.mark.it('You should use for to iterate 10 times')
+def use_for_loop(capsys):
     path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
     with open(path, 'r') as content_file:
         content = content_file.read()
-        pattern = r"print\s*\(\*get_allStudentColors\(\)\s*\)"
+        pattern = r"for\s*"
         regex = re.compile(pattern)
         assert bool(regex.search(content)) == True
-
