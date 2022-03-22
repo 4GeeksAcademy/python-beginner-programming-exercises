8f8c9aa90996b4aa7f8750c0a0452bbd4e5b785b
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2022-03-22 17:49:39 +0000

    Removed variable checking on the test, exercise 5

--- exercises/05-User-Inputed-Values/test.py
+++ exercises/05-User-Inputed-Values/test.py
@@ -1,17 +1,6 @@
 import pytest,os,re,io,sys,mock,json
 path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 
-
-@pytest.mark.it('The variable age should exist')
-def test_variable_exists(capsys):
-    import app
-    try:
-        app.age
-    except AttributeError:
-        raise AttributeError("The variable age should exist")
-
-
-
 @pytest.mark.it('Use the function print()')
 def test_for_file_output(capsys):
     with open(path, 'r') as content_file:
