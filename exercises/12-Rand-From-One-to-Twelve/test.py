ea92fe49fd1d2a1b163fbe9bf9b24244a095c020
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2022-06-23 21:45:40 +0000

    Test error fixed on exercise 12

--- exercises/12-Rand-From-One-to-Twelve/test.py
+++ exercises/12-Rand-From-One-to-Twelve/test.py
@@ -9,7 +9,6 @@ import app
 import re
 path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 
-
 @pytest.mark.it('The function get_randomInt should exist')
 def test_function_existence(app):
     try:
@@ -17,12 +16,11 @@ def test_function_existence(app):
     except AttributeError:
         raise AttributeError("The function get_randomInt should exist")
 
-
 @pytest.mark.it("Check that you are setting the correct values for the randrange function.")
 def test_conditional():
     with open(path, 'r') as content_file:
         content = content_file.read()
-        pattern = r"random\.randrange+\(1,13\)"
+        pattern = r"random\.randrange\s*\(\s*1\s*,\s*13\s*\)"
         regex = re.compile(pattern)
         assert bool(regex.search(content)) == True
 
@@ -31,10 +29,9 @@ def test_print_output(capsys, app):
     result = app.get_randomInt()
     assert result >= 1 or result <= 12
 
-
 @pytest.mark.it('You must call the function inside the print() function.')
 def test_function_called_for():
     with open(path, 'r') as content_file:
         content = content_file.read()
-        regex = re.compile(r"print\s*\(\s*get_randomInt\(\)\s*\)")
+        regex = re.compile(r"print\s*\(\s*get_randomInt\s*\(\s*\)\s*\)")
         assert bool(regex.search(content)) == True
