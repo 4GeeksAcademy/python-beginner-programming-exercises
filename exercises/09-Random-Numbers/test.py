aca3408714fc76ab971018577ab2afdf0271506c
ErnestoXG <ernestox.061@gmail.com>
2022-08-01 03:16:01 +0000

    Loops iterate minimum amount of times, functions have diffeerents names, thrird test is now inclusive of 1 andd 10

--- exercises/09-Random-Numbers/test.py
+++ exercises/09-Random-Numbers/test.py
@@ -23,28 +23,28 @@ def test_function_exists():
     try:
         assert app.get_randomInt()
     except AttributeError:
-        raise AttributeError('The function "get_randomInt" should return only values from 1 through 10 inclusive')
+        raise AttributeError('The function "get_randomInt" should exist')
 
 @pytest.mark.it("get_randomInt function should return a random integer between 1 and 10")
-def test_function_exists1():
+def test_function_returns_random_integer():
     try:
         for i in range(10):
             # only asserting if value is outside the range, else it will pass
             result = app.get_randomInt()
-            assert result > 1 and result < 10
+            assert result >= 1 and result <= 10
     except AttributeError:
         raise AttributeError('The function "get_randomInt" is returning values outside the specified range')
 
 @pytest.mark.it("get_randomInt function should NOT be returning a static integer value")
 # in case user returns single value between [1 and 10]
-def test_function_exists2():
+def test_function_return_no_static():
     try:
         tries = 0
         output = app.get_randomInt()
-        for i in range(100):
+        for i in range(10):
             if output == app.get_randomInt():
                 tries+=1
                 # only asserting if value is outside the range, else it will pass
-                assert tries < 20
+        assert tries < 10
     except AttributeError:
         raise AttributeError('The function "get_randomInt" is returning a static value, not a random one')
