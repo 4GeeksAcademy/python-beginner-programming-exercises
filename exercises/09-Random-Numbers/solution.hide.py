26f95531c11e803a7093b486434c74045e57f0be
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2022-08-22 13:04:00 +0000

    Checking the tests and small details added

--- exercises/09-Random-Numbers/solution.hide.py
+++ exercises/09-Random-Numbers/solution.hide.py
@@ -1,10 +1,8 @@
 import random
 
 def get_randomInt():
-  # CHANGE ONLY THIS LINE BELOW
+	# CHANGE ONLY THIS LINE BELOW
 	random_number = random.randint(1,10)
 	return random_number
-
-
 
 print(get_randomInt())
