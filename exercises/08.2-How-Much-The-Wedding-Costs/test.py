df3afa862462fac07532596c28228524424a83c4
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-07-27 20:50:16 +0000

    made tests more flexible

--- exercises/08.2-How-Much-The-Wedding-Costs/test.py
+++ exercises/08.2-How-Much-The-Wedding-Costs/test.py
@@ -22,7 +22,7 @@ def test__between_100_and_200(capsys, app):
         app()
         captured = capsys.readouterr()
         price = 15000
-        assert "Your wedding will cost "+str(price)+" dollars\n" == captured.out
+        assert "Your wedding will cost "+str(price)+" dollars\n" in captured.out
 
 @pytest.mark.it("Between 100 and 51 guests sould be priced 10,000")
 def test_between_101_and_51(capsys, app):
@@ -30,7 +30,7 @@ def test_between_101_and_51(capsys, app):
         app()
         captured = capsys.readouterr()
         price = 10000
-        assert "Your wedding will cost "+str(price)+" dollars\n" == captured.out
+        assert "Your wedding will cost "+str(price)+" dollars\n" in captured.out
 
 
 @pytest.mark.it("Less than 50 guests sould be priced 4,000")
@@ -39,7 +39,7 @@ def test_less_than_50(capsys, app):
         app()
         captured = capsys.readouterr()
         price = 4000
-        "Your wedding will cost "+str(price)+" dollars\n" == captured.out
+        "Your wedding will cost "+str(price)+" dollars\n" in captured.out
 
 @pytest.mark.it("More than 200 should be priced 20,000")
 def test_t(capsys, app):
@@ -47,4 +47,4 @@ def test_t(capsys, app):
         app()
         captured = capsys.readouterr()
         price = 20000
-        "Your wedding will cost "+str(price)+" dollars\n" == captured.out
+        "Your wedding will cost "+str(price)+" dollars\n" in captured.out
