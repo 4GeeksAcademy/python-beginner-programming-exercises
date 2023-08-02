62f5fefcff5cc4104b352d77c610241b5811b21c
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-02 15:32:02 +0000

    fixed wrong test, first test is now back to original

--- exercises/03-Print-Variables-In-The-Console/test.py
+++ exercises/03-Print-Variables-In-The-Console/test.py
@@ -11,7 +11,7 @@ path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 @pytest.mark.it("Create a variable named 'color' with the string value red")
 def test_declare_variable():
     result = app.color
-    assert  "red" in result
+    assert  result == "red"
 
 @pytest.mark.it('Print on the console the value of the variable ')
 def test_for_printing_variable():
@@ -24,4 +24,4 @@ def test_for_printing_variable():
 @pytest.mark.it('The printed value on the console should be "red"')
 def test_for_file_output(capsys):
     captured = buffer.getvalue()
-    assert captured == "red\n" 
+    assert  "red\n" in captured 
