2b6950f37c64383f3453b36cd4060fdeb83bc2b7
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-03 19:07:02 +0000

    updated visual clue instruction

--- exercises/09-Random-Numbers/app.py
+++ exercises/09-Random-Numbers/app.py
@@ -1,7 +1,7 @@
 import random
 
 def get_randomInt():
-  	# ✅ 🢃 CHANGE ONLY THIS LINE BELOW 🢃 ✅
+  	# ✅ 🢃 CHANGE ONLY THIS ONE LINE BELOW 🢃 ✅
 	random_number = random.random()
 	return random_number
 
