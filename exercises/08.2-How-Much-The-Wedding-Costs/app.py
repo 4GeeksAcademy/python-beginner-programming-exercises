2a579882a06224da29bff95446e760f894eca64b
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-03 14:59:05 +0000

    Added visual cues for those people that may not understand English at all

--- exercises/08.2-How-Much-The-Wedding-Costs/app.py
+++ exercises/08.2-How-Much-The-Wedding-Costs/app.py
@@ -1,6 +1,12 @@
 user_input = int(input('How many people are coming to your wedding?\n'))
 
+# ❌ 🢁 DON'T CHANGE THE CODE ABOVE  🢁 ❌
+
+
 if user_input <= 50:
     price = 4000
+# ✅ ↓ Your code here ↓ ✅
 
-print('Your wedding will cost '+str(price)+' dollars')
+
+# ❌ 🢃 DON'T CHANGE THE CODE BELOW 🢃 ❌
+print('Your wedding will cost $'+str(price)+' dollars')
