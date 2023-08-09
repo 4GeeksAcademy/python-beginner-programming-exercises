5ef1accbec9ec3979716045b193dcbab495624e7
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-09 06:53:38 +0000

    removed small typo

--- exercises/16-Random-Colors-Loop/app.py
+++ exercises/16-Random-Colors-Loop/app.py
@@ -18,8 +18,7 @@ def get_color(color_number=4):
 def get_allStudentColors():
     example_color = get_color(1)
     students_array = []
-    # ✅ ↓ your loop here ↓ ✅\
-    
+    # ✅ ↓ your loop here ↓ ✅
 
 
 print(get_allStudentColors())
