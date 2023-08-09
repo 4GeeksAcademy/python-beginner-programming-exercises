1a7e78da3602f05035352ea15c3a74e8a296bce1
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-09 06:49:41 +0000

    added solution to file

---
+++ exercises/16-Random-Colors-Loop/solution.hide.py
@@ -0,0 +1,28 @@
+import random
+
+def get_color(color_number=4):
+    # making sure is a number and not a string
+    color_number = int(color_number)
+
+    switcher={
+                  0:'red',
+                  1:'yellow',
+                  2:'blue',
+                  3:'green',
+                  4:'black'
+              }
+    return switcher.get(color_number,"Invalid Color Number")
+
+# ❌ ⬆ DON'T CHANGE THE CODE ABOVE ⬆ ❌
+
+def get_allStudentColors():
+    example_color = get_color(1)
+    students_array = []
+    # ✅ ↓ your loop here ↓ ✅
+    for i in range(10):
+        students_array.append(get_color(random.randint(0,3)))
+
+    return students_array
+
+
+print(get_allStudentColors())
