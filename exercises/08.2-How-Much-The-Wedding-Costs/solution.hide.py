9f80af3f13d4ad6615130a7a1558c182b5f1f0b4
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-09 06:13:35 +0000

    added visual cues to solution file

--- exercises/08.2-How-Much-The-Wedding-Costs/solution.hide.py
+++ exercises/08.2-How-Much-The-Wedding-Costs/solution.hide.py
@@ -1,7 +1,10 @@
 user_input = int(input('How many people are coming to your wedding?\n'))
 
+# ❌ ⬆ DON'T CHANGE THE CODE ABOVE  ⬆ ❌
+
 if user_input <= 50:
     price = 4000
+# ✅ ↓ Your code here ↓ ✅
 elif user_input <= 100:
     price = 10000
 elif user_input <= 200:
@@ -9,4 +12,5 @@ elif user_input <= 200:
 else:
     price = 20000
 
+# ❌ ↓ DON'T CHANGE THE CODE BELOW ↓ ❌
 print('Your wedding will cost '+str(price)+' dollars')
