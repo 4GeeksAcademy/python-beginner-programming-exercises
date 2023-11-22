2b5a5423ffae4e2c23c31a2c4999545b1e897394
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-22 12:36:29 +0100

    Update solution.hide.py

--- exercises/08.1-Your-First-If/solution.hide.py
+++ exercises/08.1-Your-First-If/solution.hide.py
@@ -4,6 +4,6 @@ total = int(input('How much money do you have in your pocket\n'))
 if total > 100:
     print("Give me your money!")
 elif total > 50:
-    print("Buy me some coffee you cheap!")
+    print("Buy me some coffee, you cheap!")
 else:
     print("You are a poor guy, go away!")
