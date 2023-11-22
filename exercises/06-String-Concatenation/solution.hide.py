2c1348bb42f2ec6188c6d55c1aecb1bcf63cfb73
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-22 11:57:33 +0100

    Update solution.hide.py

--- exercises/06-String-Concatenation/solution.hide.py
+++ exercises/06-String-Concatenation/solution.hide.py
@@ -3,6 +3,6 @@
 my_var1 = "Hello"
 my_var2 = "World"
 
-## Don't change below this line
-the_new_string = my_var1+' '+my_var2
+## Don't change anything below this line
+the_new_string = my_var1 + ' ' + my_var2
 print(the_new_string)
