67dd5801398535c174174684789837181de1e0a6
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-22 11:57:47 +0100

    Update app.py

--- exercises/06-String-Concatenation/app.py
+++ exercises/06-String-Concatenation/app.py
@@ -1,6 +1,6 @@
 # ✅ ↓ Set the values for my_var1 and my_var2 here ↓ ✅
 
 
-## Don't change below this line
-the_new_string = my_var1+' '+my_var2
+## Don't change anything below this line
+the_new_string = my_var1 + ' ' + my_var2
 print(the_new_string)
